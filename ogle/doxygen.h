2a3a4277bf74caa5223595881fa142cf2fffddb4
daniel <daniel@orgizm.net>
2013-03-09 15:01:20 +0100

    adding heade file that defines main page of api doc

---
+++ ogle/doxygen.h
@@ -0,0 +1,16 @@
+
+#ifndef __DOXYGEN_H_
+#define __DOXYGEN_H_
+
+/// @mainpage ogle
+///
+/// @section intro Introduction
+///
+/// ogle is a portable C++ library that ....
+/// @todo introduction
+///
+/// @section contact Contact
+///
+/// daniel@orgizm.net
+
+#endif /* __DOXYGEN_H_ */
