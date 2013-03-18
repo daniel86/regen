686205bf64c80e6ee2dc45f366b6b4a2e9e5a387
daniel <daniel@orgizm.net>
2013-03-19 00:57:57 +0100

    fixing make install target

--- ogle/meshes/particle-cloud.cpp
+++ ogle/meshes/particle-cloud.cpp
@@ -61,7 +61,7 @@ ParticleCloud::ParticleCloud(GLuint numParticles, BlendMode blendMode)
 
 void ParticleCloud::createShader(ShaderState::Config &shaderCfg, const string &drawShader)
 {
-  Particles::createShader(shaderCfg, "precipitation_particles.update", drawShader);
+  Particles::createShader(shaderCfg, "cloud_particles.update", drawShader);
 }
 
 void ParticleCloud::set_particleTexture(const ref_ptr<Texture> &tex)
