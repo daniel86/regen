0ff89d1a513f7959639efae9f8243c009fc4a78c
daniel <daniel@orgizm.net>
2013-03-21 16:58:49 +0100

    update docu

--- ogle/doxygen.h
+++ ogle/doxygen.h
@@ -2,88 +2,89 @@
 #ifndef __DOXYGEN_H_
 #define __DOXYGEN_H_
 
-/// @mainpage OGLE
-///
-/// @section intro Introduction
-/// OGLE is a portable OpenGL library written in C++.
-/// The purpose of the library is to help creating
-/// real-time rendering software.
-///
-/// The engine was developed against OpenGL 3.3. You need a
-/// graphics card supporting this API. Some features from the 4.0
-/// API are also supported but optional for backwards compatibility.
-/// The engine is tested with NVIDIA and ATI drivers and on the
-/// Operating-Systems Arch Linux and Ubuntu 11.10 (ATI dropped support for my notebook
-/// graphics adapter so i had to use an old Ubuntu version).
-/// Other Unix based Operating-Systems should work from the scratch.
-/// Windows support might require some work to be done but should be generally
-/// possible too because the engine was written with portability in mind.
-///
-/// A layer that abstracts most of the OpenGL 4.0 API is used.
-/// All related types are defined in \link gl-types \endlink .
-/// ogle::RenderState wraps server side states in stacks so that it is easy
-/// possible to reset states to their initial condition.
-/// Shader loading is done using CPU-side GLSL pre-processors.
-/// First of all the pre-processors allow using include directives
-/// which are resolved using GLSW. This allows nicer management of shader
-/// resources. The pre-processors additionally support various other
-/// code manipulations (generating IO code, manipulation IO types, for directives,
-/// blending out undefined code).
-///
-/// On-top of that a hierarchical state layer is implemented.
-/// Each ogle::State can be combined with other states and added to a ogle::StateNode.
-/// ogle::StateNode's can have one parent and multiple child nodes.
-/// When the hierarchical structure is traversed each ogle::State
-/// is enabled and disabled individually. ogle::State's usually modify
-/// the current ogle::RenderState, provide input data to shader programs,
-/// define configurations used for loading shaders or act as container for other states.
-///
-/// @section features Feature List
-/// Here you find a brief list of supported features in this library.
-///
-/// - 3D audio using OpenAL
-/// - Video/Audio streaming using ffmpeg
-/// - Image loading using DevIL
-/// - Noise textures using libnoise
-/// - Font loading using FreeType
-///     - displayed using texture mapped text
-/// - Shader pre-processing
-///     - input modification (constant, uniform, attribute, instanced attribute)
-///     - support for 'include' and 'for' directive
-/// - Assimp model loading
-///     - bone animation loading
-/// - Particle engine using transform feedback
-/// - Dynamic sky scattering
-/// - Instancing
-///     - using instanced attributes
-///     - instanced bone animation using bone TBO
-/// - GPU mesh animation using transform feedback
-/// - Deferred and Direct shading
-/// - Ambient Occlusion
-/// - Volumetric fog
-/// - Simple raycasting volume renderer
-/// - Geometry picking
-///
-/// @section deps Dependency List
-/// Following you can find a list of libraries that must be installed in order
-/// to compile OGLE.
-/// - OpenGL 3.3
-/// - OpenAL
-/// - Boost
-/// - assimp
-/// - DevIL
-/// - FreeType
-/// - libav
-///
-/// In order to compile the test applications you will also need to install
-/// the following list of libraries:
-/// - Qt >=4.0 (QtCore, QtGui, QtOpenGL)
-///
-/// @section contact Contact
-/// daniel@orgizm.net
-///
-
 /**
+@mainpage OGLE
+
+@page intro Introduction
+
+OGLE is a portable OpenGL library written in C++.
+The purpose of the library is to help creating
+real-time rendering software.
+
+The engine was developed against OpenGL 3.3. You need a
+graphics card supporting this API. Some features from the 4.0
+API are also supported but optional for backwards compatibility.
+The engine is tested with NVIDIA and ATI drivers and on the
+Operating-Systems Arch Linux and Ubuntu 11.10 (ATI dropped support for my notebook
+graphics adapter so i had to use an old Ubuntu version).
+Other Unix based Operating-Systems should work from the scratch.
+Windows support might require some work to be done but should be generally
+possible too because the engine was written with portability in mind.
+
+@section arch Brief architecture
+A layer that abstracts most of the OpenGL 4.0 API is used.
+All related types are defined in \link gl-types \endlink .
+ogle::RenderState wraps server side states in stacks so that it is easy
+possible to reset states to their initial condition.
+Shader loading is done using CPU-side GLSL pre-processors.
+First of all the pre-processors allow using include directives
+which are resolved using GLSW. This allows nicer management of shader
+resources. The pre-processors additionally support various other
+code manipulations (generating IO code, manipulation IO types, for directives,
+blending out undefined code).
+
+On-top of that a hierarchical state layer is implemented.
+Each ogle::State can be combined with other states and added to a ogle::StateNode.
+ogle::StateNode's can have one parent and multiple child nodes.
+When the hierarchical structure is traversed each ogle::State
+is enabled and disabled individually. ogle::State's usually modify
+the current ogle::RenderState, provide input data to shader programs,
+define configurations used for loading shaders or act as container for other states.
+
+@section features Feature List
+Here you find a brief list of supported features in this library.
+
+- 3D audio using OpenAL
+- Video/Audio streaming using ffmpeg
+- Image loading using DevIL
+- Noise textures using libnoise
+- Font loading using FreeType
+    - displayed using texture mapped text
+- Shader pre-processing
+    - input modification (constant, uniform, attribute, instanced attribute)
+    - support for 'include' and 'for' directive
+- Assimp model loading
+    - bone animation loading
+- Particle engine using transform feedback
+- Dynamic sky scattering
+- Instancing
+    - using instanced attributes
+    - instanced bone animation using bone TBO
+- GPU mesh animation using transform feedback
+- Deferred and Direct shading
+- Ambient Occlusion
+- Volumetric fog
+- Simple raycasting volume renderer
+- Geometry picking
+
+@section deps Dependency List
+Following you can find a list of libraries that must be installed in order
+to compile OGLE.
+- OpenGL 3.3
+- OpenAL
+- Boost
+- assimp
+- DevIL
+- FreeType
+- libav
+
+In order to compile the test applications you will also need to install
+the following list of libraries:
+- Qt >=4.0 (QtCore, QtGui, QtOpenGL)
+
+@section contact Contact
+daniel@orgizm.net
+
 @page Tutorials Tutorials
 
 @section tut_win Creating a window
