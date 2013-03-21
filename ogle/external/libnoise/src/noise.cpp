877df4c2b4400a2bd17a1f6cc9a80c019ae4c16d
daniel <daniel@orgizm.net>
2013-03-21 17:25:46 +0100

    use single noise src file

---
+++ ogle/external/libnoise/src/noise.cpp
@@ -0,0 +1,43 @@
+/*
+ * noise.cpp
+ *
+ *  Created on: 21.03.2013
+ *      Author: daniel
+ */
+
+#include "noisegen.cpp"
+#include "latlon.cpp"
+#include "model/line.cpp"
+#include "model/plane.cpp"
+#include "model/sphere.cpp"
+#include "model/cylinder.cpp"
+#include "module/abs.cpp"
+#include "module/add.cpp"
+#include "module/billow.cpp"
+#include "module/blend.cpp"
+#include "module/cache.cpp"
+#include "module/checkerboard.cpp"
+#include "module/clamp.cpp"
+#include "module/const.cpp"
+#include "module/curve.cpp"
+#include "module/cylinders.cpp"
+#include "module/displace.cpp"
+#include "module/exponent.cpp"
+#include "module/invert.cpp"
+#include "module/max.cpp"
+#include "module/min.cpp"
+#include "module/modulebase.cpp"
+#include "module/multiply.cpp"
+#include "module/perlin.cpp"
+#include "module/power.cpp"
+#include "module/ridgedmulti.cpp"
+#include "module/rotatepoint.cpp"
+#include "module/scalebias.cpp"
+#include "module/scalepoint.cpp"
+#include "module/select.cpp"
+#include "module/spheres.cpp"
+#include "module/terrace.cpp"
+#include "module/translatepoint.cpp"
+#include "module/turbulence.cpp"
+#include "module/voronoi.cpp"
+//#include "win32/dllmain.cpp"
