02304ebb76bdb568dfde38dd33137640551e3253
daniel <daniel@orgizm.net>
2013-04-07 16:39:26 +0200

    256x256 is enough for smoke

--- applications/test/transparency-test.cpp
+++ applications/test/transparency-test.cpp
@@ -263,7 +263,7 @@ int main(int argc, char** argv)
 
 #ifdef USE_PARTICLE_FOG
   ref_ptr<FrameBufferObject> particleFBO = ref_ptr<FrameBufferObject>::manage(
-      new FrameBufferObject(512, 512, 1, GL_NONE, GL_NONE, GL_NONE));
+      new FrameBufferObject(256, 256, 1, GL_NONE, GL_NONE, GL_NONE));
   ref_ptr<Texture> particleTex = particleFBO->addTexture(
       1, GL_TEXTURE_2D, GL_RGBA, GL_RGBA16F, GL_FLOAT);
 
