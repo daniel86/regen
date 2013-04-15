b972ab3187e9b74c862d7de238d732d68012a6c3
daniel <daniel@orgizm.net>
2013-04-15 17:44:47 +0200

    building static regen library because linking of shared and static libs not allowed. all shared libs and static libs are linked to the regen lib.

--- CMakeModules/FindDevIL.cmake
+++ CMakeModules/FindDevIL.cmake
@@ -3,10 +3,12 @@ include(Utility)
 
 find_include_path(DEVIL NAMES IL/il.h)
 
-find_library_path(IL ENV DEVIL_DIR NAMES libIL.a libIL libDEVIL IL DEVIL)
-find_library_path(ILU ENV DEVIL_DIR NAMES libILU.a libILU ILU ilu)
+find_library_path(DEVIL NAMES libIL libDEVIL IL DEVIL)
+find_library_path(ILU ENV DEVIL_DIR NAMES libILU ILU ilu)
+
+set(DEVIL_LIBRARIES ${DEVIL_LIBRARIES} ${ILU_LIBRARIES})
 
 # handle the QUIETLY and REQUIRED arguments and set XXX_FOUND to TRUE if all listed variables are TRUE
 include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(IL DEFAULT_MSG IL_LIBRARIES ILU_LIBRARIES DEVIL_INCLUDE_DIRS)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(IL DEFAULT_MSG HAS_DEVIL HAS_ILU DEVIL_INCLUDE_DIRS)
 
