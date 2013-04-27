da3e27b3f8666c19660daa8ff0995268f98d8a28
Daniel Beßler <daniel@orgizm.net>
2013-04-27 15:33:28 +0300

    Update FindOpenAL.cmake

--- CMakeModules/FindOpenAL.cmake
+++ CMakeModules/FindOpenAL.cmake
@@ -1,13 +1,12 @@
-
 include(Utility)
 
 # FIXME: on mac it's include/OpenAL/*.h
 find_include_path(OPENAL NAMES AL/al.h)
 
 if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-    find_library_path(OPENAL NAMES libopenal.a OpenAL al openal OpenAL64)
+    find_library_path(OPENAL NAMES libopenal.a OpenAL al openal soft_oal OpenAL64)
 else()
-    find_library_path(OPENAL NAMES libopenal.a OpenAL al openal OpenAL32)
+    find_library_path(OPENAL NAMES libopenal.a OpenAL al openal soft_oal OpenAL32)
 endif()
 
 # handle the QUIETLY and REQUIRED arguments and set XXX_FOUND to TRUE if all listed variables are TRUE
