763369760bbfa3388ef085254018ab8230714722
daniel <danielb@orgizm.net>
2013-04-30 16:52:07 +0200

    My laptop does not provide GL_ARB_texture_buffer_range. Make it optional.

--- regen/gl-types/tbo.cpp
+++ regen/gl-types/tbo.cpp
@@ -22,12 +22,16 @@ void TextureBufferObject::attach(const ref_ptr<VertexBufferObject> &vbo, VBORefe
 {
   attachedVBO_ = vbo;
   attachedVBORef_ = ref;
+#ifdef GL_ARB_texture_buffer_range
   glTexBufferRange(
       targetType_,
       texelFormat_,
       ref->bufferID(),
       ref->address(),
       ref->allocatedSize());
+#else
+  glTexBuffer(targetType_, texelFormat_, ref->bufferID());
+#endif
   GL_ERROR_LOG();
 }
 void TextureBufferObject::attach(GLuint storage)
@@ -41,7 +45,11 @@ void TextureBufferObject::attach(GLuint storage, GLuint offset, GLuint size)
 {
   attachedVBO_ = ref_ptr<VertexBufferObject>();
   attachedVBORef_ = VBOReference();
+#ifdef GL_ARB_texture_buffer_range
   glTexBufferRange(targetType_, texelFormat_, storage, offset, size);
+#else
+  glTexBuffer(targetType_, texelFormat_, storage);
+#endif
   GL_ERROR_LOG();
 }
 
