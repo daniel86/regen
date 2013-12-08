218b8c1e312f61dc5d0ca9ecaa2e31ee5289ea8b
daniel <daniel@orgizm.net>
2013-12-08 20:05:56 +0100

    Adding ReflectionCamera class that can be used for rendering the reflected scene to texture.

--- regen/states/camera.cpp
+++ regen/states/camera.cpp
@@ -7,6 +7,7 @@
 
 #include <GL/glew.h>
 
+#include <regen/states/atomic-states.h>
 #include <regen/math/vector.h>
 #include <regen/math/matrix.h>
 #include <regen/av/audio.h>
@@ -131,3 +132,156 @@ void Camera::set_isAudioListener(GLboolean isAudioListener)
 }
 GLboolean Camera::isAudioListener() const
 { return isAudioListener_; }
+
+/////////////////////////////
+/////////////////////////////
+
+ReflectionCamera::ReflectionCamera(
+    const ref_ptr<Camera> &userCamera,
+    const ref_ptr<Mesh> &mesh,
+    GLuint vertexIndex)
+: Camera(),
+  userCamera_(userCamera),
+  vertexIndex_(vertexIndex),
+  projStamp_(userCamera->projection()->stamp()-1),
+  camPosStamp_(userCamera->position()->stamp()-1),
+  camDirStamp_(userCamera->direction()->stamp()-1),
+  cameraChanged_(GL_TRUE),
+  isFront_(GL_TRUE)
+{
+  frustum_->setProjection(
+      userCamera->frustum()->fov()->getVertex(0),
+      userCamera->frustum()->aspect()->getVertex(0),
+      userCamera->frustum()->near()->getVertex(0),
+      userCamera->frustum()->far()->getVertex(0));
+
+  clipPlane_ = ref_ptr<ShaderInput4f>::alloc("clipPlane");
+  clipPlane_->setUniformData(Vec4f(0.0f));
+  setInput(clipPlane_);
+
+  pos_ = mesh->positions();
+  nor_ = mesh->normals();
+  isReflectorValid_ = (pos_.get()!=NULL) && (nor_.get()!=NULL);
+  if(isReflectorValid_) {
+    posStamp_ = pos_->stamp()-1;
+    norStamp_ = nor_->stamp()-1;
+  }
+
+  transform_ = mesh->findShaderInput("modelMatrix");
+  if(transform_.get()!=NULL) {
+    transformStamp_ = transform_->stamp()-1;
+  }
+}
+
+void ReflectionCamera::enable(RenderState *rs)
+{
+  if(!isReflectorValid_) {
+    REGEN_WARN("Reflector has no position/normal attribute.");
+  }
+  else if(!isHidden()) {
+    updateReflection();
+    Camera::enable(rs);
+  }
+}
+
+void ReflectionCamera::updateReflection()
+{
+  GLboolean reflectorChanged = GL_FALSE;
+  if(transform_.get()!=NULL && transform_->stamp()!=transformStamp_) {
+    reflectorChanged = GL_TRUE;
+    transformStamp_ = transform_->stamp();
+  }
+  if(nor_->stamp()!=norStamp_) {
+    reflectorChanged = GL_TRUE;
+    norStamp_ = nor_->stamp();
+  }
+  if(pos_->stamp()!=posStamp_) {
+    reflectorChanged = GL_TRUE;
+    posStamp_ = pos_->stamp();
+  }
+  // Compute plane parameters...
+  if(reflectorChanged) {
+    if(!pos_->hasClientData()) pos_->readServerData();
+    if(!nor_->hasClientData()) nor_->readServerData();
+    posWorld_ = ((Vec3f*)pos_->clientData())[vertexIndex_];
+    norWorld_ = ((Vec3f*)nor_->clientData())[vertexIndex_];
+
+    if(transform_.get()!=NULL) {
+      if(!transform_->hasClientData()) transform_->readServerData();
+      Mat4f transform = (((Mat4f*)transform_->clientData())[0]).transpose();
+      posWorld_ = transform.transformVector(posWorld_);
+      norWorld_ = transform.rotateVector(norWorld_);
+      norWorld_.normalize();
+    }
+  }
+
+  // Switch normal if viewer is behind reflector.
+  // TODO: also allow to skip computation in this case.
+  const Vec3f &camPos = userCamera_->position()->getVertex(0);
+  GLboolean isFront = norWorld_.dot(camPos-posWorld_)>0.0;
+  if(isFront != isFront_) {
+    isFront_ = isFront;
+    reflectorChanged = GL_TRUE;
+  }
+  // Compute reflection matrix...
+  if(reflectorChanged) {
+    if(isFront_) {
+      clipPlane_->setVertex(0, Vec4f(
+          norWorld_.x,norWorld_.y,norWorld_.z,
+          norWorld_.dot(posWorld_)));
+      reflectionMatrix_ = Mat4f::reflectionMatrix(posWorld_,norWorld_);
+    }
+    else {
+      // flip reflector normal
+      Vec3f n = -norWorld_;
+      clipPlane_->setVertex(0, Vec4f(n.x,n.y,n.z, n.dot(posWorld_)));
+      reflectionMatrix_ = Mat4f::reflectionMatrix(posWorld_,n);
+    }
+  }
+
+  // Compute reflection camera position
+  if(reflectorChanged || userCamera_->position()->stamp() != camPosStamp_) {
+    camPosStamp_ = userCamera_->position()->stamp();
+    position_->setVertex(0, reflectionMatrix_.transformVector(
+        userCamera_->position()->getVertex(0)));
+
+    reflectorChanged = GL_TRUE;
+  }
+  // Compute reflection camera direction
+  if(reflectorChanged || userCamera_->direction()->stamp() != camDirStamp_) {
+    camDirStamp_ = userCamera_->direction()->stamp();
+    Vec3f dir = reflectionMatrix_.rotateVector(userCamera_->direction()->getVertex(0));
+    dir.normalize();
+    direction_->setVertex(0,dir);
+
+    reflectorChanged = GL_TRUE;
+  }
+  // Compute view matrix
+  if(reflectorChanged) {
+    view_->setUniformData(Mat4f::lookAtMatrix(
+        position_->getVertex(0),
+        direction_->getVertex(0),
+        Vec3f::up()));
+    viewInv_->setUniformData(view_->getVertex(0).lookAtInverse());
+    cameraChanged_ = GL_TRUE;
+  }
+
+  // Compute projection matrix
+  if(userCamera_->projection()->stamp() != projStamp_) {
+    projStamp_ = userCamera_->projection()->stamp();
+    proj_->setUniformData(
+        userCamera_->projection()->getVertex(0));
+    projInv_->setUniformData(
+        userCamera_->projectionInverse()->getVertex(0));
+    cameraChanged_ = GL_TRUE;
+  }
+
+  // Compute view-projection matrix
+  if(cameraChanged_) {
+    viewproj_->setVertex(0,
+        view_->getVertex(0) * proj_->getVertex(0));
+    viewprojInv_->setVertex(0,
+        projInv_->getVertex(0) * viewInv_->getVertex(0));
+    cameraChanged_ = GL_FALSE;
+  }
+}
--- regen/states/camera.h
+++ regen/states/camera.h
@@ -12,6 +12,8 @@
 #include <regen/utility/ref-ptr.h>
 #include <regen/math/matrix.h>
 #include <regen/math/frustum.h>
+#include <regen/meshes/mesh-state.h>
+#include <regen/states/model-transformation.h>
 #include <regen/gl-types/shader-input-container.h>
 
 namespace regen {
@@ -105,6 +107,46 @@ namespace regen {
     ref_ptr<ShaderInputMat4> projInv_;
     ref_ptr<ShaderInputMat4> viewproj_;
     ref_ptr<ShaderInputMat4> viewprojInv_;
+  };
+
+  /**
+   * Virtual camera that reflects another camera along
+   * an arbitrary plane.
+   */
+  class ReflectionCamera : public Camera
+  {
+  public:
+    /**
+     * @param cam The user camera to reflect.
+     * @param mesh The reflector plane (first vertex and normal taken).
+     */
+    ReflectionCamera(const ref_ptr<Camera> &cam, const ref_ptr<Mesh> &mesh, GLuint vertexIndex=0);
+
+    // Override
+    virtual void enable(RenderState *rs);
+
+  protected:
+    ref_ptr<Camera> userCamera_;
+    ref_ptr<ShaderInput> pos_;
+    ref_ptr<ShaderInput> nor_;
+    ref_ptr<ShaderInput> transform_;
+    ref_ptr<ShaderInput4f> clipPlane_;
+    ref_ptr<State> cullState_;
+    Vec3f posWorld_;
+    Vec3f norWorld_;
+    GLuint vertexIndex_;
+    GLuint projStamp_;
+    GLuint posStamp_;
+    GLuint norStamp_;
+    GLuint camPosStamp_;
+    GLuint camDirStamp_;
+    GLuint transformStamp_;
+    GLboolean cameraChanged_;
+    GLboolean isReflectorValid_;
+    GLboolean isFront_;
+    Mat4f reflectionMatrix_;
+
+    void updateReflection();
   };
 } // namespace
 
