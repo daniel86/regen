1f1012b8bb620213f5794f4505b07fb97e8b37e3
daniel <daniel@orgizm.net>
2013-12-13 20:26:04 +0100

    Allowing to turn of vignette and radial blur effect in tonemap shader

--- regen/glsl/regen/post-passes/tonemap.glsl
+++ regen/glsl/regen/post-passes/tonemap.glsl
@@ -15,19 +15,26 @@ const float in_blurAmount = 0.5;
 const float in_effectAmount = 0.2;
 const float in_exposure = 16.0;
 const float in_gamma = 0.5;
+#ifdef USE_RADIAL_BLUR
 const float in_radialBlurSamples = 30.0;
 const float in_radialBlurStartScale = 1.0;
 const float in_radialBlurScaleMul = 0.9;
+#endif
+#ifdef USE_VIGNETTE
 const float in_vignetteInner = 0.7;
 const float in_vignetteOuter = 1.5;
+#endif
 
 out vec4 out_color;
 
+#ifdef USE_VIGNETTE
 float vignette(vec2 pos, float inner, float outer)
 {
   return 1.0 - smoothstep(inner, outer, length(pos));
 }
+#endif
 
+#ifdef USE_RADIAL_BLUR
 vec4 radialBlur(sampler2D tex, vec2 texcoord, int samples,
         float startScale, float scaleMul)
 {
@@ -42,6 +49,7 @@ vec4 radialBlur(sampler2D tex, vec2 texcoord, int samples,
     c /= samples;
     return c;
 }
+#endif
 
 void main() {
     // sum original and blurred image
@@ -49,16 +57,22 @@ void main() {
         texture(in_inputTexture, in_texco),
         texture(in_blurTexture, in_texco), in_blurAmount
     );
+#ifdef USE_RADIAL_BLUR
     out_color += in_effectAmount * radialBlur(
             in_blurTexture, in_texco,
             int(in_radialBlurSamples),
             in_radialBlurStartScale,
             in_radialBlurScaleMul);
+#endif
+#ifdef USE_VIGNETTE
     // exposure and vignette effect
     out_color *= in_exposure * vignette(
         in_texco*2.0-vec2(1.0),
         in_vignetteInner,
         in_vignetteOuter);
+#else
+    out_color *= in_exposure;
+#endif
     // gamma correction
     out_color.rgb = pow(out_color.rgb, vec3(in_gamma));
 }
