a2b13134fd8c7f4af7a0d65ec901aac81c469ea9
Daniel Beßler <danielb@uni-bremen.de>
2024-10-17 17:22:02 +0200

    Code cleanup

--- regen/av/video-stream.h
+++ regen/av/video-stream.h
@@ -12,58 +12,65 @@
 #include <regen/av/av-stream.h>
 
 namespace regen {
-  /**
-   * \brief ffmpeg stream that provides texture data for GL texture.
-   */
-  class VideoStream : public AudioVideoStream
-  {
-  public:
-    /**
-     * @param stream the stream object.
-     * @param index the stream index.
-     * @param chachedBytesLimit size limit for pre loading.
-     */
-    VideoStream(AVStream *stream, GLint index, GLuint chachedBytesLimit);
-    virtual ~VideoStream();
+	/**
+	 * \brief ffmpeg stream that provides texture data for GL texture.
+	 */
+	class VideoStream : public AudioVideoStream {
+	public:
+		/**
+		 * @param stream the stream object.
+		 * @param index the stream index.
+		 * @param cachedBytesLimit size limit for pre loading.
+		 */
+		VideoStream(AVStream *stream, GLint index, GLuint cachedBytesLimit);
 
-    /**
-     * The stream handle as provided to the constructor.
-     */
-    AVStream *stream();
+		~VideoStream() override;
 
-    /**
-     * Video width in pixels.
-     */
-    GLint width() const;
-    /**
-     * Video height in pixels.
-     */
-    GLint height() const;
+		/**
+		 * The stream handle as provided to the constructor.
+		 */
+		AVStream *stream();
 
-    /**
-     * Format for GL texture to match frame data.
-     */
-    static GLenum texInternalFormat() ;
-    /**
-     * Format for GL texture to match frame data.
-     */
-    static GLenum texFormat() ;
-    /**
-     * Pixel type for GL texture to match frame data.
-     */
-    static GLenum texPixelType() ;
+		/**
+		 * Video width in pixels.
+		 */
+		GLint width() const;
 
-    // override
-    void decode(AVPacket *packet);
-    void clearQueue();
+		/**
+		 * Video height in pixels.
+		 */
+		GLint height() const;
 
-  protected:
-    struct SwsContext *swsCtx_;
-    AVFrame *currFrame_;
+		/**
+		 * Format for GL texture to match frame data.
+		 */
+		static GLenum texInternalFormat();
 
-    AVStream *stream_;
-    GLint width_, height_;
-  };
+		/**
+		 * Format for GL texture to match frame data.
+		 */
+		static GLenum texFormat();
+
+		/**
+		 * Pixel type for GL texture to match frame data.
+		 */
+		static GLenum texPixelType();
+
+		// override
+		void decode(AVPacket *packet) override;
+
+		// override
+		void clearQueue() override;
+
+	protected:
+		struct SwsContext *swsCtx_;
+		AVFrame *currFrame_;
+
+		AVStream *stream_;
+		GLint width_, height_;
+
+		void doClearQueue();
+	};
 } // namespace
 
 #endif /* VIDEO_STREAM_H_ */
