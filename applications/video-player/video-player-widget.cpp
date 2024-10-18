505cb7d00fb05df6f53bf3e4c32f44a180fc74b9
Daniel Beßler <danielb@uni-bremen.de>
2024-10-18 09:28:42 +0200

    Need to initialize GL resources solely in render thread with GL context

--- applications/video-player/video-player-widget.cpp
+++ applications/video-player/video-player-widget.cpp
@@ -8,6 +8,13 @@
 #include <iostream>
 
 #include "video-player-widget.h"
+#include "regen/gl-types/fbo.h"
+#include <regen/states/texture-state.h>
+#include <regen/states/shader-state.h>
+#include <regen/states/fbo-state.h>
+#include <regen/states/blit-state.h>
+#include <regen/meshes/rectangle.h>
+#include <regen/states/state-configurer.h>
 
 #include <QtWidgets/QFileDialog>
 #include <QtGui/QMouseEvent>
@@ -80,6 +87,16 @@ static GLboolean isDirectory(const string &f)
 ////////////
 ////////////
 
+class VideoInitAnimation : public Animation {
+public:
+  explicit VideoInitAnimation(VideoPlayerWidget *widget)
+  : Animation(GL_TRUE,GL_FALSE),  widget_(widget) {}
+
+  void glAnimate(RenderState *rs, GLdouble dt) override { widget_->gl_loadScene(); }
+
+  VideoPlayerWidget *widget_;
+};
+
 VideoPlayerWidget::VideoPlayerWidget(QtApplication *app)
 : QMainWindow(),
   app_(app),
@@ -118,6 +135,95 @@ VideoPlayerWidget::VideoPlayerWidget(QtApplication *app)
   elapsedTimer_.start();
 
   srand(time(NULL));
+
+  initAnim_ = ref_ptr<VideoInitAnimation>::alloc(this);
+}
+
+// Resizes Framebuffer texture when the window size changed
+class FBOResizer : public EventHandler {
+public:
+  FBOResizer(const ref_ptr<FBOState> &fbo, GLfloat wScale, GLfloat hScale)
+  : EventHandler(), fboState_(fbo), wScale_(wScale), hScale_(hScale) { }
+
+  void call(EventObject *evObject, EventData*) {
+    Application *app = (Application*)evObject;
+    const Vec2i& winSize = app->windowViewport()->getVertex(0);
+    fboState_->resize(winSize.x*wScale_, winSize.y*hScale_);
+  }
+
+protected:
+  ref_ptr<FBOState> fboState_;
+  GLfloat wScale_, hScale_;
+};
+
+void setBlitToScreen(Application *app, const ref_ptr<FBO> &fbo, GLenum attachment) {
+  ref_ptr<State> blitState = ref_ptr<BlitToScreen>::alloc(fbo, app->windowViewport(), attachment);
+  app->renderTree()->addChild(ref_ptr<StateNode>::alloc(blitState));
+}
+
+ref_ptr<Mesh> createVideoWidget(
+    Application *app,
+    const ref_ptr<Texture> &videoTexture,
+    const ref_ptr<StateNode> &root)
+{
+  Rectangle::Config quadConfig;
+  quadConfig.levelOfDetail = 0;
+  quadConfig.isTexcoRequired = GL_TRUE;
+  quadConfig.isNormalRequired = GL_FALSE;
+  quadConfig.isTangentRequired = GL_FALSE;
+  quadConfig.centerAtOrigin = GL_TRUE;
+  quadConfig.rotation = Vec3f(0.5*M_PI, 0.0*M_PI, 0.0*M_PI);
+  quadConfig.posScale = Vec3f(1.0f);
+  quadConfig.texcoScale = Vec2f(-1.0f, 1.0f);
+  quadConfig.levelOfDetail = 0;
+  quadConfig.isTexcoRequired = GL_TRUE;
+  quadConfig.isNormalRequired = GL_FALSE;
+  quadConfig.centerAtOrigin = GL_TRUE;
+  ref_ptr<Mesh> mesh = ref_ptr<regen::Rectangle>::alloc(quadConfig);
+
+  ref_ptr<TextureState> texState = ref_ptr<TextureState>::alloc(videoTexture);
+  texState->set_mapTo(TextureState::MAP_TO_COLOR);
+  mesh->joinStates(texState);
+
+  ref_ptr<ShaderState> shaderState = ref_ptr<ShaderState>::alloc();
+  mesh->joinStates(shaderState);
+
+  ref_ptr<StateNode> meshNode = ref_ptr<StateNode>::alloc(mesh);
+  root->addChild(meshNode);
+
+  StateConfigurer shaderConfigurer;
+  shaderConfigurer.addNode(meshNode.get());
+  shaderConfigurer.define("USE_NORMALIZED_COORDINATES", "TRUE");
+  shaderState->createShader(shaderConfigurer.cfg(), "regen.gui.widget");
+  mesh->updateVAO(RenderState::get(), shaderConfigurer.cfg(), shaderState->shader());
+
+  return mesh;
+}
+
+void VideoPlayerWidget::gl_loadScene() {
+	AnimationManager::get().pause(GL_TRUE);
+
+  // create render target
+  const Vec2i& winSize = app_->windowViewport()->getVertex(0);
+  ref_ptr<FBO> fbo = ref_ptr<FBO>::alloc(winSize.x, winSize.y);
+  ref_ptr<Texture> target = fbo->addTexture(1, GL_TEXTURE_2D, GL_RGB, GL_RGB, GL_UNSIGNED_BYTE);
+  ref_ptr<FBOState> fboState = ref_ptr<FBOState>::alloc(fbo);
+  fboState->addDrawBuffer(GL_COLOR_ATTACHMENT0);
+  // resize fbo with window
+  app_->connect(Application::RESIZE_EVENT, ref_ptr<FBOResizer>::alloc(fboState,1.0,1.0));
+
+  // create a root node (that binds the render target)
+  ref_ptr<StateNode> sceneRoot = ref_ptr<StateNode>::alloc(fboState);
+  app_->renderTree()->addChild(sceneRoot);
+
+  // add the video widget to the root node
+  createVideoWidget(app_, video(), sceneRoot);
+  setBlitToScreen(app_, fbo, GL_COLOR_ATTACHMENT0);
+  GL_ERROR_LOG()
+
+  initAnim_ = ref_ptr<Animation>();
+  AnimationManager::get().resume();
+  REGEN_INFO("Video Scene Loaded.");
 }
 
 const ref_ptr<VideoTexture>& VideoPlayerWidget::video() const
