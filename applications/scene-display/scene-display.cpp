ebc8776579c29455119bd6cc7e0b16ef077fd2be
Daniel Beßler <danielb@uni-bremen.de>
2024-10-19 12:54:06 +0200

    reformat

--- applications/scene-display/scene-display.cpp
+++ applications/scene-display/scene-display.cpp
@@ -7,6 +7,7 @@
 #ifdef Q_WS_X11
 #include <X11/Xlib.h>
 #endif
+
 #include <QtWidgets/QFileDialog>
 #include <QtCore/QString>
 
@@ -16,45 +17,44 @@
 
 using namespace regen;
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
 #ifdef Q_WS_X11
 #ifndef SINGLE_THREAD_GUI_AND_GRAPHICS
-  XInitThreads();
+	XInitThreads();
 #endif
 #endif
-  QGLFormat glFormat(
-    QGL::SingleBuffer
-   |QGL::NoAlphaChannel
-   |QGL::NoAccumBuffer
-   |QGL::NoDepthBuffer
-   |QGL::NoStencilBuffer
-   |QGL::NoStereoBuffers
-   |QGL::NoSampleBuffers);
-  glFormat.setSwapInterval(0);
-  glFormat.setDirectRendering(true);
-  glFormat.setRgba(true);
-  glFormat.setOverlay(false);
-  glFormat.setVersion(3,3);
-  glFormat.setProfile(QGLFormat::CoreProfile);
+	QGLFormat glFormat(
+			QGL::SingleBuffer
+			| QGL::NoAlphaChannel
+			| QGL::NoAccumBuffer
+			| QGL::NoDepthBuffer
+			| QGL::NoStencilBuffer
+			| QGL::NoStereoBuffers
+			| QGL::NoSampleBuffers);
+	glFormat.setSwapInterval(0);
+	glFormat.setDirectRendering(true);
+	glFormat.setRgba(true);
+	glFormat.setOverlay(false);
+	glFormat.setVersion(3, 3);
+	glFormat.setProfile(QGLFormat::CoreProfile);
 
-  // create and show application window
-  ref_ptr<QtApplication> app = ref_ptr<QtApplication>::alloc(argc,(const char**)argv,glFormat);
-  app->setupLogging();
+	// create and show application window
+	ref_ptr<QtApplication> app = ref_ptr<QtApplication>::alloc(argc, (const char **) argv, glFormat);
+	app->setupLogging();
 
-  // add a custom path for shader loading
-  boost::filesystem::path shaderPath(REGEN_SOURCE_DIR);
-  shaderPath /= "applications";
-  shaderPath /= "scene-display";
-  shaderPath /= "shader";
-  app->addShaderPath(shaderPath.string());
+	// add a custom path for shader loading
+	boost::filesystem::path shaderPath(REGEN_SOURCE_DIR);
+	shaderPath /= "applications";
+	shaderPath /= "scene-display";
+	shaderPath /= "shader";
+	app->addShaderPath(shaderPath.string());
 
-  auto *widget = new SceneDisplayWidget(app.get());
-  app->show();
-  widget->show();
-  widget->init();
+	auto *widget = new SceneDisplayWidget(app.get());
+	app->show();
+	widget->show();
+	widget->init();
 
-  widget->setWindowTitle("Scene Viewer");
+	widget->setWindowTitle("Scene Viewer");
 
-  return app->mainLoop();
+	return app->mainLoop();
 }
