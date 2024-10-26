e3663869969f37b34af10ef891da16fe22732bdf
Daniel Beßler <danielb@uni-bremen.de>
2024-10-26 20:30:25 +0200

    cleanup

---
+++ regen/sky/sky-layer.cpp
@@ -0,0 +1,23 @@
+#include "regen/sky/sky-layer.h"
+#include "regen/sky/sky.h"
+
+using namespace regen;
+
+SkyLayer::SkyLayer(const ref_ptr<Sky> &sky) {
+	sky_ = sky;
+	updateInterval_ = 4000.0;
+	dt_ = updateInterval_;
+}
+
+SkyLayer::~SkyLayer() = default;
+
+void SkyLayer::updateSky(RenderState *rs, GLdouble dt) {
+	dt_ += dt;
+	if (dt_ < updateInterval_) { return; }
+
+	GL_ERROR_LOG();
+	updateSkyLayer(rs, dt_);
+	GL_ERROR_LOG();
+
+	dt_ = 0.0;
+}
