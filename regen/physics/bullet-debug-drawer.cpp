58ae4da1718a571d7d59662a3450cb05b99ec10f
Daniel Beßler <danielb@uni-bremen.de>
2024-10-31 18:08:59 +0100

    rather control linear velocity with character controller

--- regen/physics/bullet-debug-drawer.cpp
+++ regen/physics/bullet-debug-drawer.cpp
@@ -3,19 +3,19 @@
 using namespace regen;
 
 BulletDebugDrawer::BulletDebugDrawer(const ref_ptr<BulletPhysics> &physics)
-		: StateNode(),
+		: btIDebugDraw(),
+		  StateNode(),
 		  HasShader("regen.models.lines"),
 		  HasInput(VBO::USAGE_DYNAMIC),
 		  physics_(physics),
 		  vbo_(0),
-		  m_debugMode(DBG_DrawWireframe){
+		  m_debugMode(DBG_DrawContactPoints | DBG_DrawWireframe){
 	lineColor_ = ref_ptr<ShaderInput3f>::alloc("lineColor");
 	lineColor_->setUniformData(Vec3f(1.0f));
 	state()->joinShaderInput(lineColor_);
 	state()->joinStates(shaderState_);
 	lineVertices_ = ref_ptr<ShaderInput3f>::alloc("lineVertices");
 	lineVertices_->setVertexData(2);
-
 	// Create and set up the VAO and VBO
 	vao_ = ref_ptr<VAO>::alloc();
 	bufferSize_ = sizeof(GLfloat) * 3 * lineVertices_->numVertices();
@@ -38,10 +38,33 @@ void BulletDebugDrawer::drawLine(const btVector3 &from, const btVector3 &to, con
 }
 
 void
-BulletDebugDrawer::drawContactPoint(const btVector3 &PointOnB, const btVector3 &normalOnB, btScalar distance,
-									int lifeTime,
-									const btVector3 &color)
-{}
+BulletDebugDrawer::drawContactPoint(
+		const btVector3 &PointOnB,
+		const btVector3 &normalOnB,
+		btScalar distance,
+		int lifeTime,
+		const btVector3 &color)
+{
+    // Set the color for the contact point
+    lineColor_->setUniformData(Vec3f(color.getX(), color.getY(), color.getZ()));
+    // Calculate the end point of the normal vector
+    btVector3 to = PointOnB + normalOnB * distance;
+    // Update CPU-side vertex data for the contact point
+    lineVertices_->setVertex(0,
+    	Vec3f(PointOnB.getX(), PointOnB.getY(), PointOnB.getZ()));
+    lineVertices_->setVertex(1,
+    	Vec3f(to.getX(), to.getY(), to.getZ()));
+    // Update GPU-side vertex data
+    glBufferData(GL_ARRAY_BUFFER,
+    		bufferSize_,
+    		lineVertices_->clientData(),
+    		GL_DYNAMIC_DRAW);
+    // Draw the contact point as a small line
+    renderState_->vao().push(vao_->id());
+    lineVertices_->enableAttribute(0);
+    glDrawArrays(GL_LINES, 0, 2);
+    renderState_->vao().pop();
+}
 
 void BulletDebugDrawer::reportErrorWarning(const char *warningString) {
 	REGEN_WARN("Bullet Warning: " << warningString);
