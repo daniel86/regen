6703ecf670b2b717a44ff8d335c9dfd5a17373f0
Daniel Beßler <danielb@uni-bremen.de>
2024-11-11 22:13:07 +0100

    revisited particles, made shaders more configurable and re-usable

--- regen/glsl/regen/particles/sprite.glsl
+++ regen/glsl/regen/particles/sprite.glsl
@@ -46,6 +46,9 @@ out vec3 out_posEye;
 out vec3 out_posWorld;
 out vec2 out_texco0;
 out float out_lifetime;
+#ifdef HAS_color
+out vec3 out_color;
+#endif
 
 #include regen.states.camera.input
 #include regen.states.camera.transformWorldToEye
@@ -61,6 +64,9 @@ void main() {
 
     out_velocity = in_velocity[0];
     out_lifetime = in_lifetime[0];
+#ifdef HAS_color
+    out_color = in_color[0];
+#endif
     
     vec4 centerEye = transformWorldToEye(vec4(in_pos[0],1.0),0);
     vec3 quadPos[4] = computeSpritePoints(centerEye.xyz, vec2(in_size[0]), vec3(0.0, 1.0, 0.0));
@@ -69,6 +75,7 @@ void main() {
 
 -- fs
 #include regen.models.mesh.defines
+#include regen.states.textures.defines
 
 layout(location = 0) out vec4 out_color;
 
@@ -77,26 +84,40 @@ in vec3 in_posWorld;
 in vec3 in_velocity;
 in vec2 in_texco0;
 in float in_lifetime;
+#ifdef HAS_color
+in vec3 in_color;
+#endif
 
-const float in_softParticleScale = 1.0;
 const float in_particleBrightness = 0.5;
-
-uniform sampler2D in_particleTexture;
+const float in_lifetimeSmoothstep = 1.0;
+#ifdef USE_NEAR_CAMERA_SOFT_PARTICLES
+const float in_cameraSmoothstep = 5.0;
+#endif
 
 uniform vec3 in_cameraPosition;
 uniform vec2 in_viewport;
+
 #ifdef USE_SOFT_PARTICLES
 #include regen.states.camera.input
 uniform sampler2D in_depthTexture;
 #endif
 
-#include regen.shading.direct.diffuse
+#ifdef USE_SOFT_PARTICLES
 #include regen.states.camera.linearizeDepth
 #include regen.particles.sprite.softParticleScale
+const float in_softParticleScale = 1.0;
+#endif
+
+#if ${NUM_LIGHTS} > 0
+    #include regen.shading.direct.diffuse
+#endif
+#ifdef HAS_FRAGMENT_TEXTURE
+    #include regen.states.textures.mapToFragmentUnshaded
+#endif
 
 void main() {
     vec3 P = in_posWorld.xyz;
-    float opacity = in_particleBrightness;
+    float opacity = 1.0;
     
 #ifdef USE_SOFT_PARTICLES
     // fade out particles intersecting the world
@@ -104,16 +125,30 @@ void main() {
 #endif
 #ifdef USE_NEAR_CAMERA_SOFT_PARTICLES
     // fade out particls near camera
-    opacity *= smoothstep(0.0, 5.0, distance(P, in_cameraPosition));
+    opacity *= smoothstep(0.0, in_cameraSmoothstep, distance(P, in_cameraPosition));
 #endif
-    // fade out based on texture intensity
-    opacity *= texture(in_particleTexture, in_texco0).x;
     // fade out based on lifetime
-    // TODO: take other conditions of death into account, like below surface
-    opacity *= smoothstep(0.0, 1.0, in_lifetime/20.0);
+    opacity *= smoothstep(0.0, in_lifetimeSmoothstep, in_lifetime);
     if(opacity<0.0001) discard;
-    
+
+#ifdef HAS_color
+    out_color = vec4(in_color.rgb, 1.0);
+#elif HAS_particleColor
+    out_color = vec4(in_particleColor.rgb, 1.0);
+#else
+    out_color = vec4(1.0, 1.0, 1.0, 1.0);
+#endif
+#ifdef HAS_FRAGMENT_TEXTURE
+    textureMappingFragmentUnshaded(P, out_color);
+#endif
+#if ${NUM_LIGHTS} > 0
     vec3 diffuseColor = getDiffuseLight(P, gl_FragCoord.z);
-    out_color = vec4(diffuseColor,1.0);
+    out_color.rgb *= diffuseColor;
+#endif
+    // apply the brightness of the particle
+    out_color.rgb *= in_particleBrightness;
+    out_color.a *= opacity;
+#ifdef OPACITY_WEIGHTED_COLOR
     out_color.rgb *= opacity; // opacity weighted color
+#endif
 }
