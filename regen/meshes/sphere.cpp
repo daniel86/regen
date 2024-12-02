9fba6952d1bf327b6ecad1581c96f50f50ba425c
Daniel Beßler <danielb@uni-bremen.de>
2024-12-02 21:33:39 +0100

    sphere uv mapping

--- regen/meshes/sphere.cpp
+++ regen/meshes/sphere.cpp
@@ -17,10 +17,6 @@ namespace regen {
 				return out << "NONE";
 			case Sphere::TEXCO_MODE_UV:
 				return out << "UV";
-			case Sphere::TEXCO_MODE_SPHERICAL:
-				return out << "SPHERICAL";
-			case Sphere::TEXCO_MODE_SPHERE_MAP:
-				return out << "SPHERE_MAP";
 		}
 		return out;
 	}
@@ -31,8 +27,6 @@ namespace regen {
 		boost::to_upper(val);
 		if (val == "NONE") mode = Sphere::TEXCO_MODE_NONE;
 		else if (val == "UV") mode = Sphere::TEXCO_MODE_UV;
-		else if (val == "SPHERICAL") mode = Sphere::TEXCO_MODE_SPHERICAL;
-		else if (val == "SPHERE_MAP") mode = Sphere::TEXCO_MODE_SPHERE_MAP;
 		else {
 			REGEN_WARN("Unknown sphere texco mode '" << val << "'. Using NONE texco.");
 			mode = Sphere::TEXCO_MODE_NONE;
@@ -61,7 +55,7 @@ Sphere::Config::Config()
 		: posScale(Vec3f(1.0f)),
 		  texcoScale(Vec2f(1.0f)),
 		  levelOfDetail(4),
-		  texcoMode(TEXCO_MODE_SPHERICAL),
+		  texcoMode(TEXCO_MODE_UV),
 		  isNormalRequired(GL_TRUE),
 		  isTangentRequired(GL_FALSE),
 		  isHalfSphere(GL_FALSE),
@@ -85,7 +79,10 @@ static GLboolean isOnPosYSphereEntirely(TriangleVertex *triangle) {
 static Vec3f computeSphereTangent(const Vec3f &v) {
 	Vec3f vAbs = Vec3f(abs(v.x), abs(v.y), abs(v.z));
 	Vec3f v_;
-	if (vAbs.x < vAbs.y && vAbs.x < vAbs.z) {
+	if (1.0f - v.z < std::numeric_limits<float>::epsilon()) {
+		// there is a singularity at the back pole
+		v_ = Vec3f(1.0, 0.0, 0.0);
+	} else if (vAbs.x < vAbs.y && vAbs.x < vAbs.z) {
 		v_ = Vec3f(0.0, -v.z, v.y);
 	} else if (vAbs.y < vAbs.x && vAbs.y < vAbs.z) {
 		v_ = Vec3f(-v.z, 0, v.x);
@@ -96,76 +93,40 @@ static Vec3f computeSphereTangent(const Vec3f &v) {
 	return v.cross(v_);
 }
 
+void Sphere::pushVertex(GLuint vertexIndex, GLdouble u, GLdouble v, const Config &cfg) {
+	auto positions = ((Vec3f*) pos_->clientData());
+
+	GLdouble r = std::sin(M_PI * v);
+	positions[vertexIndex] = Vec3f(
+		static_cast<float>(r * std::cos(2.0 * M_PI * u)),
+		static_cast<float>(r * std::sin(2.0 * M_PI * u)),
+		static_cast<float>(std::cos(M_PI * v))
+	);
+	if (cfg.isNormalRequired) {
+		nor_->setVertex(vertexIndex, positions[vertexIndex]);
+	}
+	if (cfg.isTangentRequired) {
+		Vec3f t = computeSphereTangent(positions[vertexIndex]);
+		tan_->setVertex(vertexIndex, Vec4f(t.x, t.y, t.z, 1.0));
+	}
+	if (texco_.get()) {
+		auto texco = (Vec2f *) texco_->clientData();
+		texco[vertexIndex] = Vec2f(
+				static_cast<float>(u) * cfg.texcoScale.x,
+				static_cast<float>(v) * cfg.texcoScale.y);
+	}
+	positions[vertexIndex] *= cfg.posScale * 0.5;
+}
+
 void Sphere::updateAttributes(const Config &cfg) {
-	std::vector<TriangleFace> faces;
-	{
-		// setup initial level
-		GLfloat a = 1.0 / sqrt(2.0) + 0.001;
-
-		TriangleVertex level0[6];
-		level0[0] = TriangleVertex(Vec3f(0.0f, 0.0f, 1.0f), 0);
-		level0[1] = TriangleVertex(Vec3f(0.0f, 0.0f, -1.0f), 1);
-		level0[2] = TriangleVertex(Vec3f(-a, -a, 0.0f), 2);
-		level0[3] = TriangleVertex(Vec3f(a, -a, 0.0f), 3);
-		level0[4] = TriangleVertex(Vec3f(a, a, 0.0f), 4);
-		level0[5] = TriangleVertex(Vec3f(-a, a, 0.0f), 5);
-
-		std::vector<TriangleFace> facesLevel0(8);
-		facesLevel0[0] = TriangleFace(level0[0], level0[3], level0[4]);
-		facesLevel0[1] = TriangleFace(level0[0], level0[4], level0[5]);
-		facesLevel0[2] = TriangleFace(level0[0], level0[5], level0[2]);
-		facesLevel0[3] = TriangleFace(level0[0], level0[2], level0[3]);
-		facesLevel0[4] = TriangleFace(level0[1], level0[4], level0[3]);
-		facesLevel0[5] = TriangleFace(level0[1], level0[5], level0[4]);
-		facesLevel0[6] = TriangleFace(level0[1], level0[2], level0[5]);
-		facesLevel0[7] = TriangleFace(level0[1], level0[3], level0[2]);
-		faces = tessellate(cfg.levelOfDetail, facesLevel0);
-	}
-
-	// Find out number of triangle faces
-	GLuint faceCounter = 0;
-	if (cfg.isHalfSphere) {
-		for (GLuint faceIndex = 0; faceIndex < faces.size(); ++faceIndex) {
-			TriangleFace &face = faces[faceIndex];
-			auto *vertices = (TriangleVertex *) &face;
-			if (isOnPosYSphereEntirely(vertices)) continue;
-			for (GLuint i = 0; i < 3; ++i) {
-				if (vertices[i].p.y > 0) vertices[i].p.y = 0.0;
-			}
-			faceCounter += 1;
-		}
+	GLuint stepSize = 4 + cfg.levelOfDetail * cfg.levelOfDetail;
+	GLuint numFaces;
+	if(cfg.isHalfSphere) {
+		numFaces = stepSize * stepSize;
 	} else {
-		faceCounter = faces.size();
+		numFaces = 2 * stepSize * stepSize;
 	}
-	// Allocate RAM for indices
-	GLuint numIndices = faceCounter * 3;
-	ref_ptr<ShaderInput1ui> indices = ref_ptr<ShaderInput1ui>::alloc("i");
-	indices->setVertexData(numIndices);
-	auto *indicesPtr = (GLuint *) indices->clientDataPtr();
-
-	// Set index data and compute vertex count
-	std::map<GLuint, GLint> indexMap;
-	GLuint currIndex = 0;
-	for (GLuint faceIndex = 0; faceIndex < faces.size(); ++faceIndex) {
-		TriangleFace &face = faces[faceIndex];
-		auto *vertices = (TriangleVertex *) &face;
-		if (cfg.isHalfSphere && isOnPosYSpherePartially(vertices)) continue;
-
-		for (GLuint i = 0; i < 3; ++i) {
-			TriangleVertex &vertex = vertices[i];
-			// Find vertex index
-			if (indexMap.count(vertex.i) == 0) {
-				indexMap[vertex.i] = currIndex;
-				currIndex += 1;
-			}
-			// Add to indices attribute
-			*indicesPtr = indexMap[vertex.i];
-			indicesPtr += 1;
-		}
-		faceCounter += 1;
-	}
-	// Number of vertices is equal to number of distinctive indices.
-	GLuint numVertices = indexMap.size();
+	GLuint numVertices = numFaces * 2;
 
 	// allocate attributes
 	pos_->setVertexData(numVertices);
@@ -175,79 +136,46 @@ void Sphere::updateAttributes(const Config &cfg) {
 	if (cfg.isTangentRequired) {
 		tan_->setVertexData(numVertices);
 	}
-
 	TexcoMode texcoMode = cfg.texcoMode;
 	if (cfg.isTangentRequired && cfg.texcoMode == TEXCO_MODE_NONE) {
 		texcoMode = TEXCO_MODE_UV;
 	}
-	if (texcoMode == TEXCO_MODE_SPHERE_MAP) {
-		texco_ = ref_ptr<ShaderInput2f>::alloc("texco0");
-		texco_->setVertexData(numVertices);
-	} else if (texcoMode == TEXCO_MODE_UV || texcoMode == TEXCO_MODE_SPHERICAL) {
+	if (texcoMode == TEXCO_MODE_UV) {
 		texco_ = ref_ptr<ShaderInput2f>::alloc("texco0");
 		texco_->setVertexData(numVertices);
 	}
 
-	for (GLuint faceIndex = 0; faceIndex < faces.size(); ++faceIndex) {
-		TriangleFace &face = faces[faceIndex];
-		auto *vertices = (TriangleVertex *) &face;
-		if (cfg.isHalfSphere && isOnPosYSpherePartially(vertices)) continue;
+	// Allocate RAM for indices
+	GLuint numIndices = numFaces * 3;
+	ref_ptr<ShaderInput1ui> indices = ref_ptr<ShaderInput1ui>::alloc("i");
+	indices->setVertexData(numIndices);
+	auto *indicesPtr = (GLuint *) indices->clientDataPtr();
 
-		// Compute uv-factors for this face
-		GLfloat s1, s2, s3, t1, t2, t3;
-		if (texcoMode == TEXCO_MODE_SPHERE_MAP) {
-			sphereUV(vertices[0].p, &s1, &t1);
-			sphereUV(vertices[1].p, &s2, &t2);
-			if (s2 < 0.75 && s1 > 0.75) {
-				s2 += 1.0;
-			} else if (s2 > 0.75 && s1 < 0.75) {
-				s2 -= 1.0;
-			}
+	GLdouble stepSizeInv = 1.0 / (GLdouble) stepSize;
+	GLuint vertexIndex = 0, faceIndex = 0;
+	for (GLuint i=0; i<stepSize; i++) {
+		for (GLuint j=0; j<stepSize; j++) {
+            GLdouble u0 = (GLdouble)i * stepSizeInv;
+            GLdouble u1 = (GLdouble)(i + 1) * stepSizeInv;
+            GLdouble v0 = (GLdouble)j * stepSizeInv;
+            GLdouble v1 = (GLdouble)(j + 1) * stepSizeInv;
 
-			sphereUV(vertices[2].p, &s3, &t3);
-			if (s3 < 0.75 && s2 > 0.75) {
-				s3 += 1.0;
-			} else if (s3 > 0.75 && s2 < 0.75) {
-				s3 -= 1.0;
-			}
-		}
+            if (cfg.isHalfSphere && u0 < 0.5) continue;
 
-		// Compute index and attribute values
-		for (GLuint i = 0; i < 3; ++i) {
-			TriangleVertex &vertex = vertices[i];
-			GLint vertexIndex = indexMap[vertex.i];
-			if (vertexIndex == -1) continue;
-			indexMap[vertex.i] = -1;
+            // create two triangles for each quad
+            GLuint index = (faceIndex++) * 6;
+            indicesPtr[index + 0] = vertexIndex + 0;
+            indicesPtr[index + 1] = vertexIndex + 1;
+            indicesPtr[index + 2] = vertexIndex + 2;
+            indicesPtr[index + 3] = vertexIndex + 2;
+            indicesPtr[index + 4] = vertexIndex + 1;
+            indicesPtr[index + 5] = vertexIndex + 3;
 
-			vertex.p.normalize();
-			pos_->setVertex(vertexIndex, cfg.posScale * vertex.p * 0.5);
-			if (cfg.isNormalRequired) {
-				nor_->setVertex(vertexIndex, vertex.p);
-			}
-			if (cfg.isTangentRequired) {
-				Vec3f t = computeSphereTangent(pos_->getVertex(vertexIndex));
-				tan_->setVertex(vertexIndex, Vec4f(t.x, t.y, t.z, 1.0));
-			}
-			if (texcoMode == TEXCO_MODE_SPHERICAL) {
-				auto *texco = (Vec2f *) texco_->clientData();
-				texco[vertexIndex].x = (0.5 + (atan2(vertex.p.z, vertex.p.x) / (2.0 * M_PI)));
-				texco[vertexIndex].y = (0.5 - (asin(vertex.p.y) / M_PI));
-				// Ensure UV coordinates wrap around correctly
-				if (texco[vertexIndex].x < 0.0f) texco[vertexIndex].x += 1.0f;
-				if (texco[vertexIndex].x >= 1.0f) texco[vertexIndex].x -= 1.0f;
-			} else if (texcoMode == TEXCO_MODE_UV) {
-				auto *texco = (Vec2f *) texco_->clientData();
-				auto *pos = (Vec3f *) pos_->clientData();
-				texco[vertexIndex] = Vec2f(
-						0.5f + pos[vertexIndex].x / cfg.posScale.x,
-						0.5f + pos[vertexIndex].y / cfg.posScale.y
-				);
-			} else if (texcoMode == TEXCO_MODE_SPHERE_MAP) {
-				auto *texco = (Vec2f *) texco_->clientData();
-				if (i == 0) texco[vertexIndex] = Vec2f(s1, t1);
-				else if (i == 1) texco[vertexIndex] = Vec2f(s2, t2);
-				else if (i == 2) texco[vertexIndex] = Vec2f(s3, t3);
-			}
+            // they are made of 4 vertices
+            pushVertex(vertexIndex++, u1, v1, cfg);
+            pushVertex(vertexIndex++, u1, v0, cfg);
+            pushVertex(vertexIndex++, u0, v1, cfg);
+            pushVertex(vertexIndex++, u0, v0, cfg);
 		}
 	}
 
