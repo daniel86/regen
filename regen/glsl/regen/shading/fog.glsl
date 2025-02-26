bbcf71d739eda4ced1e5bf9a4885f17516a02d7e
Daniel Beßler <danielb@uni-bremen.de>
2025-02-26 22:05:43 +0100

    improved character controller scene

--- regen/glsl/regen/shading/fog.glsl
+++ regen/glsl/regen/shading/fog.glsl
@@ -51,10 +51,9 @@ void main() {
     vecTexco texco = computeTexco(texco_2D);
     
     float d0 = texture(in_gDepthTexture, texco).x;
-    if(d0==1.0) discard; // discard background pixels
     vec3 eye0 = transformTexcoToWorld(texco_2D, d0, in_layer) - in_cameraPosition;
     float eye0Length = length(eye0);
-    float factor0 = fogIntensity(eye0Length);
+    float factor0 = float(d0<1.0) * fogIntensity(eye0Length);
 
     vec3 fogColor = in_fogColor;
 #ifdef HAS_skyColorTexture
