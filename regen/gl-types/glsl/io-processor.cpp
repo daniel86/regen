8f448edc018987a5ad090b6b2edb0909e1ad2243
Daniel Beßler <danielb@uni-bremen.de>
2025-04-25 10:57:15 +0200

    fix for TBO matrix data in culling shader, needed to introduce a new macro "fetch_$inputName(i)" which gives mat4 in case of modelMatrix input.

--- regen/gl-types/glsl/io-processor.cpp
+++ regen/gl-types/glsl/io-processor.cpp
@@ -310,11 +310,17 @@ void IOProcessor::declareSpecifiedInput(PreProcessorState &state) {
 				io.block.push_back(memberIO.declaration());
 				inputNames_.insert(blockNameWithoutPrefix);
 
-				if (blockUniform.in_->numInstances() > 1 && currStage_ != GL_COMPUTE_SHADER) {
-					lineQueue_.push_back(REGEN_STRING("#define in_" << blockNameWithoutPrefix <<
-						" instances_" << blockNameWithoutPrefix << "[regen_InstanceID]"));
-					lineQueue_.push_back(REGEN_STRING("#define fetch_" << blockNameWithoutPrefix <<
-						"(i) instances_" << blockNameWithoutPrefix << "[i]"));
+				if (blockUniform.in_->numInstances() > 1) {
+					if (currStage_ == GL_COMPUTE_SHADER) {
+						lineQueue_.push_back(REGEN_STRING("#define fetch_" << blockNameWithoutPrefix <<
+							"(i) in_" << blockNameWithoutPrefix << "[i]"));
+					}
+					else {
+						lineQueue_.push_back(REGEN_STRING("#define in_" << blockNameWithoutPrefix <<
+							" instances_" << blockNameWithoutPrefix << "[regen_InstanceID]"));
+						lineQueue_.push_back(REGEN_STRING("#define fetch_" << blockNameWithoutPrefix <<
+							"(i) instances_" << blockNameWithoutPrefix << "[i]"));
+					}
 				}
 			}
 			uniforms_[state.currStage].insert(make_pair(nameWithoutPrefix, io));
