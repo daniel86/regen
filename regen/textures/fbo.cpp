d9ca53c1a1071e32276a6184b8ece643b4c08d8e
Daniel Beßler <danielb@uni-bremen.de>
2025-08-13 19:56:21 +0200

    minor

--- regen/textures/fbo.cpp
+++ regen/textures/fbo.cpp
@@ -1,7 +1,6 @@
 #include "regen/gl-types/render-state.h"
 #include "regen/gl-types/gl-util.h"
 #include "regen/glsl/shader-input.h"
-#include "cmake-build-debug/regen/config.h"
 
 #include "fbo.h"
 #include "regen/scene/scene.h"
